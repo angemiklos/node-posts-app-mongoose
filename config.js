4e1fa658de1b9bdadaa0aee9ec1f0bfffb2bb49f
Ange Miklos <angejuba@gmail.com>
2018-05-15 13:18:53 -0700

    initial checkin of integration test challenge

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/posts-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-posts-app';
-exports.PORT = process.env.PORT || 8080;
+export const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/posts-app';
+export const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-posts-app';
+export const PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,56 +1,42 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
-
-// Mongoose internally uses a promise-like object,
-// but its better to make Mongoose use built in es6 promises
 mongoose.Promise = global.Promise;
 
-// config.js is where we control constants for entire
-// app like PORT and DATABASE_URL
-const { PORT, DATABASE_URL } = require('./config');
-const { Post } = require('./models');
+const { DATABASE_URL, PORT } = require('./config');
+const { BlogPost } = require('./models');
 
 const app = express();
+
+app.use(morgan('common'));
 app.use(express.json());
 
-// GET requests to /posts => return all blog posts
 app.get('/posts', (req, res) => {
-  Post
+  BlogPost
     .find()
-    // success callback: for each blog post we got back, we'll
-    // call the `.serialize` instance method we've created in
-    // models.js in order to only expose the data we want the API return.
     .then(posts => {
-      res.json({
-        posts: posts.map(
-          (post) => post.serialize())
-      });
+      res.json(posts.map(post => post.serialize()));
     })
     .catch(err => {
       console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
+      res.status(500).json({ error: 'something went terribly wrong' });
     });
 });
 
-// can also request by ID
 app.get('/posts/:id', (req, res) => {
-  Post
-    // this is a convenience method Mongoose provides for searching
-    // by the object _id property
+  BlogPost
     .findById(req.params.id)
     .then(post => res.json(post.serialize()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
+      res.status(500).json({ error: 'something went horribly awry' });
     });
 });
 
-
 app.post('/posts', (req, res) => {
-
-  const requiredFields = ['tite', 'content', 'author'];
+  const requiredFields = ['title', 'content', 'author'];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -60,58 +46,66 @@ app.post('/posts', (req, res) => {
     }
   }
 
-  Post
+  BlogPost
     .create({
-      title: req.body.name,
-      author: req.body.borough,
-      content: req.body.cuisine,
-      created: req.body.grades
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author
     })
-    .then(post => res.status(201).json(post.serialize()))
+    .then(blogPost => res.status(201).json(blogPost.serialize()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
+      res.status(500).json({ error: 'Something went wrong' });
+    });
+
+});
+
+
+app.delete('/posts/:id', (req, res) => {
+  BlogPost
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).json({ message: 'success' });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went terribly wrong' });
     });
 });
 
 
 app.put('/posts/:id', (req, res) => {
-  // ensure that the id in the request path and the one in request body match
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id ` +
-      `(${req.body.id}) must match`);
-    console.error(message);
-    return res.status(400).json({ message: message });
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
   }
 
-  // we only support a subset of fields being updateable.
-  // if the user sent over any of the updatableFields, we udpate those values
-  // in document
-  const toUpdate = {};
+  const updated = {};
   const updateableFields = ['title', 'content', 'author'];
-
   updateableFields.forEach(field => {
     if (field in req.body) {
-      toUpdate[field] = req.body[field];
+      updated[field] = req.body[field];
     }
   });
 
-  Post
-    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
-    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
-    .then(post => res.status(204).end())
-    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+  BlogPost
+    .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+    .then(updatedPost => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Something went wrong' }));
 });
 
-app.delete('/posts/:id', (req, res) => {
-  Post
+
+app.delete('/:id', (req, res) => {
+  BlogPost
     .findByIdAndRemove(req.params.id)
-    .then(post => res.status(204).end())
-    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+    .then(() => {
+      console.log(`Deleted blog post with id \`${req.params.id}\``);
+      res.status(204).end();
+    });
 });
 
-// catch-all endpoint if client makes request to non-existent endpoint
+
 app.use('*', function (req, res) {
   res.status(404).json({ message: 'Not Found' });
 });
@@ -122,8 +116,7 @@ app.use('*', function (req, res) {
 let server;
 
 // this function connects to our database, then starts the server
-function runServer(databaseUrl=DATABASE_URL, port = PORT) {
-
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(databaseUrl, err => {
       if (err) {
@@ -163,4 +156,4 @@ if (require.main === module) {
   runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
-module.exports = { app, runServer, closeServer };
+module.exports = { runServer, app, closeServer };
